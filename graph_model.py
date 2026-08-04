801fc625953e1faac320c971ee0ee3f6eba0d1bd
Raquel de Frutos <username@users.noreply.github.com>
2026-08-04 23:29:40 +0200

    update

--- graph_model.py
+++ graph_model.py
@@ -212,7 +212,8 @@ def main():
     #nodes1
     # graph_dir ="/home/rdefrutos/motion_gnn_events/data/MVSEC_outdoorday1/nodes/nodes/*.npz"
     #nodes2
-    graph_dir ="/home/rdefrutos/motion_gnn_events/data/MVSEC_outdoorday1/nodesPose/*.npz"
+    # graph_dir ="/home/rdefrutos/motion_gnn_events/data/MVSEC_outdoorday1/nodesPose/*.npz"
+    graph_dir ="/home/rdefrutos/motion_gnn_events/data/MVSEC_outdoorday1/nodesPose16/*.npz"
     
     # graph_dir ="/home/rdefrutos/motion_gnn_events/data/MVSEC_indoorflying1/nodesPose/nodesPose/*.npz"
     graph_files_raw = glob.glob(graph_dir)
